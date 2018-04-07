d8084cf88c0da2e4b54d3d49580ffc5f021a1d7d
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2018-04-07 15:24:08 +0800

    buat method cetakinfo()

--- src/praktek03/Helm.java
+++ src/praktek03/Helm.java
@@ -5,4 +5,10 @@ public class Helm {
     String merk;
     String warna;
     double harga;
+    
+    void PakaiHelm(){
+         System.out.println("Merk \t: "+merk+"\n"+
+             "warna \t: "+warna+"\n"+
+             "harga \t: "+harga);
+    }
 }
