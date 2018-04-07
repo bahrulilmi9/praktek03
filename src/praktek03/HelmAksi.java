11b19fa75ad75f020f63884916b92d26a1f32e43
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2018-04-07 15:44:19 +0800

    menggunakan method set

--- src/praktek03/HelmAksi.java
+++ src/praktek03/HelmAksi.java
@@ -2,9 +2,10 @@ package praktek03;
 public class HelmAksi {
     public static void main(String[] args){
         Helm j = new Helm();
-        j.merk="NHK";
-        j.warna="Hitam";
-        j.harga=300;
+        
+        j.setMerk("NHK");
+        j.setWarna("Hitam");
+        j.setHarga(300);
         
         j.PakaiHelm();
     }
