a60b3628bdd6d817342322593aa4c23c99a2f5e4
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2018-04-07 15:15:36 +0800

    Membuat attribut

--- src/praktek03/Helm.java
+++ src/praktek03/Helm.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package praktek03;
 
-/**
- *
- * @author ACER ASPIRE E5-475
- */
 public class Helm {
-    
+    String merk;
+    String warna;
+    double harga;
 }
