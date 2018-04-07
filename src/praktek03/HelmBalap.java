4358e461d2c0ee0a68e44ce29e0a9d803b7286af
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2018-04-07 16:15:13 +0800

    Getter and Setter

--- src/praktek03/HelmBalap.java
+++ src/praktek03/HelmBalap.java
@@ -6,4 +6,14 @@ public class HelmBalap extends Helm {
     void throttle(){
         System.out.println("WEEEEEEYYYYYYY. . . !!!");
     }
+
+    public String getJenisHelm() {
+        return JenisHelm;
+    }
+
+    public void setJenisHelm(String JenisHelm) {
+        this.JenisHelm = JenisHelm;
+    }
+    
+    
 }
