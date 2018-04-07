54ab706bce551806d46378902ee3f18ce6714b4c
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2018-04-07 15:33:21 +0800

    Instance, buat attribut dan panggil method

--- src/praktek03/HelmAksi.java
+++ src/praktek03/HelmAksi.java
@@ -1,6 +1,11 @@
 package praktek03;
 public class HelmAksi {
     public static void main(String[] args){
+        Helm j = new Helm();
+        j.merk="NHK";
+        j.warna="Hitam";
+        j.harga=300;
         
+        j.PakaiHelm();
     }
 }
