708bbf7211b12b73b7584e8dc5e1cc9e1a991ecb
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2018-04-07 16:13:32 +0800

    attribute dan method

--- src/praktek03/HelmBalap.java
+++ src/praktek03/HelmBalap.java
@@ -1,5 +1,9 @@
 package praktek03;
 
 public class HelmBalap extends Helm {
+    private String JenisHelm;
     
+    void throttle(){
+        System.out.println("WEEEEEEYYYYYYY. . . !!!");
+    }
 }
